ccdaf88666b06517ff22c1455ab1624d367bf51f
joeltok <jttctc@gmail.com>
2017-05-25 12:23:05 +0800

    Refactor to use JavaScript Module Pattern

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-var SocketResponseTester = {};
+var SocketResponseTester = (function() {
+	return {
 
-SocketResponseTester.fire = function(sockets, promiseFactory) {
+		fire: function(sockets, promiseFactory) {
 
-	var that = this;
-	var resolves = {};
-	var storedPromises = [];
+			var that = this;
+			var resolves = {};
+			var storedPromises = [];
 
-	return Promise.all(sockets.map((socket, index) => {
-		return new Promise((resolve, reject) => {
-			var listener = (data) => {
-				socket.removeListener('message', listener);
-				// use the resolves from the later stage
-				resolves[index](data);
-			}
-			socket.on('message', listener);
-			resolve(true);
-		})
-	}))
-	.then(() => {
-		sockets.forEach((socket, index) => {
-			// this mechanism creates promises whose resolves are stored for later use
-			storedPromises.push(new Promise((resolve, reject) => {
-				resolves[index] = resolve;
+			return Promise.all(sockets.map((socket, index) => {
+				return new Promise((resolve, reject) => {
+					var listener = (data) => {
+						socket.removeListener('message', listener);
+						// use the resolves from the later stage
+						resolves[index](data);
+					}
+					socket.on('message', listener);
+					resolve(true);
+				})
 			}))
-		})
-		return true;
-	})
-	.then(() => {
-		return promiseFactory();
-	})
-	.then(() => {
-		// at this point, wait for all stored promises to resolve, then return their values
-		return Promise.all(storedPromises);
-	})
+			.then(() => {
+				sockets.forEach((socket, index) => {
+					// this mechanism creates promises whose resolves are stored for later use
+					storedPromises.push(new Promise((resolve, reject) => {
+						resolves[index] = resolve;
+					}))
+				})
+				return true;
+			})
+			.then(() => {
+				return promiseFactory();
+			})
+			.then(() => {
+				// at this point, wait for all stored promises to resolve, then return their values
+				return Promise.all(storedPromises);
+			})
 
-};
+		}
+
+	}
+}())
 
 module.exports = SocketResponseTester;
