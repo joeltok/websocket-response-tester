4571a92b4997e064db77cc3dc592d0532cd57dcb
joeltok <jttctc@gmail.com>
2017-07-24 17:30:37 +0800

    test: Socket array with one event

--- test/index.js
+++ test/index.js
@@ -86,14 +86,15 @@ describe('Test builder', function() {
 	})
 
 	it('# Socket array with one event', function(done) {
-		wrt.build()
-		.addEventWaiter([donkeys.pink, donkeys.teal], 'message')
+		wrt()
+		.registerSockets(donkeys)
+		.addEventWaiter(['pink','teal'], 'message')
 		.queueFunction(() => {
 			donkeys.blue.emit('message', 'brayyyy')
 		})
 		.then((responses) => {
-			assert.equal(responses[donkeys.pink.io.engine.id]['message'][0], 'brayyyy')
-			assert.equal(responses[donkeys.teal.io.engine.id]['message'][0], 'brayyyy')
+			assert.equal(responses['pink']['message'][0], 'brayyyy')
+			assert.equal(responses['pink']['message'][0], 'brayyyy')
 			done();
 		})
 	})
